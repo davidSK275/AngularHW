62ecd4067a5a7312b7558f48f68b9cacb7d304f8
David Kim <dkim212@gmu.edu>
2023-03-03 19:56:53 -0500

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Moe Bagheri', role: 'Designer/Client-side Programmer' },
+  { id: 2, name: 'Alex Elson', role: 'Scrum Master' },
+  { id: 3, name: 'Mohammad S', role: 'Product Owner' },
+  { id: 4, name: 'Michael Basazinew', role: 'Server-Side Programmer' },
+  { id: 5, name: 'David Kim', role: 'Database Designer and Programmer' },
+  { id: 6, name: 'Tim Nguyen', role: 'builder' }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
